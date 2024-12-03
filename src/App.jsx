bd1b43acc69559a95101b39ad98bfff64919d6b7
sakib <sbsakibsarkar8@gmail.com>
2024-12-03 21:13:05 +0600

    done all section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbers from './component/navber/Navber'
 import Allocate from './page/Home/Allocate'
 import Cards from './page/Home/Cards'
 import Embrace from './page/Home/Embrace'
+import Footer from './page/Home/Footer'
 import Hero from './page/Home/Hero'
 import Partners from './page/Home/Partners'
 import Sliders from './page/Home/Slider'
@@ -26,6 +27,7 @@ function App() {
    <Sliders/>
    <Allocate/>
    <Embrace/>
+   <Footer/>
    </>
   )
 }
---
+++ src/page/Home/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className=' bg-[#07292F] w-full h-auto py-10'>
+        <div className='footer_main'> 
+            <div>
+                <img src="img/Logo.png" alt="" />
+            </div>
+            <div>
+                <p className='footer_p'>Copyright © 2024 Wizia. All rights reserved. sb sakib sarkar </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
